ccf32d14aad2a45cfe708d4f132e2932f54d70c2
001binary <56477215+001binary@users.noreply.github.com>
2023-02-20 12:49:41 +0100

    First commit

---
+++ home.js
@@ -0,0 +1,22 @@
+!<DOCTYPE html>
+<html>
+<body>
+
+<h2>My First Web Page</h2>
+<p>My First Paragraph.</p>
+
+<p id="demo"></p>
+<tr> 
+    <td>School</td>
+    <td> <input type="text" name="school" id "school" class ="lng1"></td>
+</tr>
+<tr> 
+    <td colspan= "2"> <center> <button></button></center></td>
+</tr>
+
+<script>
+document.getElementById("demo").innerHTML = 5 + 6;
+</script>
+
+</body>
+</html> 
